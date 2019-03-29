7bf3e51876dee1f33b0301d9c714401e164235cc
fbinabri <f.binabri@gmail.com>
2019-03-29 11:50:54 +0400

    Corrections

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -774,7 +774,7 @@
  *      O-- FRONT --+
  *    (0,0)
  */
-#define X_PROBE_OFFSET_FROM_EXTRUDER 110  // X offset: -left  +right  [of the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER 10  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -0.45   // Z offset: -below +above  [the nozzle] (FSR Nozzle offset)
 
